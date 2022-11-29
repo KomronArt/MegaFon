72dfb8a54adfc7d608ac14a065e93931521f3057
KomronArt <65872176+KomronArt@users.noreply.github.com>
2022-11-29 23:09:53 +0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <TheHeader />
-    <TheMain />
+    <TheMain 
+      v-bind:LeftItems = LeftItems
+      v-bind:RightItems = RightItems
+    />
   </div>
 </template>
 
@@ -14,7 +17,20 @@ export default {
   components: {
     TheHeader,
     TheMain,
-  }
+  },
+  data(){
+    return {
+      LeftItems:[
+        {id: 1, title: "Текущий баланс:", value: "12,00 TJS",option:"+ Пополнить" },
+        ],
+      RightItems: [
+        {id: 2, title: "Ваш тариф:", value: "Биё! Фаъол бош - Сотрудник",option:"Сменить тариф" },
+        {id: 3, title: "Остатки минут по РТ:", value: "102 минуты",option:"Подключай минуты +" },
+        {id: 4, title: "Остатки интернет трафика:", value: "987 МБ",option:"Подключай мегабайты +" },
+        {id: 5, title: "Остатки бонусных баллов:", value: "150",option:"Обменять" },
+              ]
+            }
+        },
 }
 </script>
 
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,33 +1,28 @@
 <template>
     <div class="flex justify-center bg-stone-200 gap-10 pb-10">
-        <div class="bg-white rounded-xl h-36 -mt-10 w-1/4">
-            <div class="h-32 leading-9">
-                <p class="text-neutral-400 pl-10 pt-4">Текущий баланс:</p>
-                <p class="text-3xl font-extrabold text-zinc-800 pl-10">12,00 TJS</p>
-                <a href="#" class="text-cyan-400 text-sm font-semibold pl-10 ">+ Пополнить</a>
-            </div>
-        </div>
+        <LeftSection 
+            v-for="LeftItem of LeftItems" :key="LeftItem"
+            v-bind:LeftItem = LeftItem
+        />
+        
         <div class="bg-white rounded-xl -mt-10 w-1/2">
-            <div class="h-32 leading-9">
-                <p class="text-neutral-400 pl-10 pt-4">Ваш тариф:</p>
-                <p class="text-3xl font-extrabold text-zinc-800 pl-10">Биё! Фаъол бош - Сотрудник</p>
-                <a href="" class="text-cyan-400 text-sm font-semibold pl-10">Сменить тариф</a>
-            </div>
-            <div class="h-32 leading-9">
-                <p class="text-neutral-400 pl-10 pt-4">Остатки минут по РТ:</p>
-                <p class="text-3xl font-extrabold text-zinc-800 pl-10">102 минуты</p>
-                <a href="" class="text-cyan-400 text-sm font-semibold pl-10">Подключай минуты +</a>
-            </div>
-            <div class="h-32 leading-9">
-                <p class="text-neutral-400 pl-10 pt-4">Остатки интернет трафика:</p>
-                <p class="text-3xl font-extrabold text-zinc-800 pl-10">987 МБ</p>
-                <a href="" class="text-cyan-400 text-sm font-semibold pl-10">Подключай мегабайты +</a>
-            </div>
-            <div class="h-32 leading-9">
-                <p class="text-neutral-400 pl-10 pt-4">Остатки бонусных баллов:</p>
-                <p class="text-3xl font-extrabold text-zinc-800 pl-10">150</p>
-                <a href="" class="text-cyan-400 text-sm font-semibold pl-10">Обменять</a>
-            </div>
+            <RightSection 
+                v-for="RightItem of RightItems" :key="RightItem"
+                v-bind:RightItem = RightItem
+            />
         </div>
     </div>
 </template>
+
+<script>
+    import LeftSection from './LeftSection.vue';
+    import RightSection from './RightSection.vue';
+
+    export default{
+        components:{
+    LeftSection,
+    RightSection
+},
+        props: ["LeftItems", "RightItems"],
+    }
+</script>
