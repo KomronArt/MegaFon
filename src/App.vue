1a7e9519de3473dc72e232349123b926d7d01a89
KomronArt <65872176+KomronArt@users.noreply.github.com>
2022-11-29 21:17:41 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <TheHeader />
+    <TheMain />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+export default {
+  name: 'App',
+
+  components: {
+    TheHeader,
+    TheMain,
+  }
+}
+</script>
+
+<style>
+#app{
+    margin: 0;
+    padding: 0;
+    font-family:sans-serif;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="bg-green-500 flex justify-between text-white  p-20">
+        <div class="logo-section">
+            <img class="w-36" src="../assets/logo.png" alt="Logo">
+        </div>
+        <div class="leading-5 text-sm">
+            <p class="user">+992 90 0000 000</p>
+            <p class="user">Фамилия Имя Отчество</p>
+        </div>
+    </header>
+</template>
